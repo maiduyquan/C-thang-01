549d2d85dd647adaa08023fd45553faca788f13b
maiduyquan <91303493+maiduyquan@users.noreply.github.com>
2022-06-23 00:08:47 +0700

    Add files via upload

---
+++ First_prg.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Example
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("This Is the my first program using C#");
+        Console.ReadLine();
+    }
+}
